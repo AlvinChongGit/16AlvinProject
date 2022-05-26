6d77e6aea36dd077c8e50a966c2b0de2e475ed0c
Alvin <chong_zhee_wey@connect.ite.edu.sg>
2022-05-26 09:26:40 +0800

    First Update
    Update of 7 initial files

---
+++ nav.php
@@ -0,0 +1,8 @@
+<div id="banner" align="center"><img src="banner.png" width="1400" height="200" alt="banner"/></div>
+<link href="style.css" rel="stylesheet" type="text/css">
+<div class="topnav">
+	<a href="logcall.php">Log Call</a>
+	<a href="update.php">Update</a>
+	<a href="#">Report </a>
+	<a href="#">History</a>
+</div>
